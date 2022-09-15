6b2f2377f323e350a9bef65a7c434da34c01cfeb
Your NameTitín <titin.colomer.romero@gmail.com>
2022-09-15 18:41:21 +0200

    subir estilo al buscador de productos

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,6 +4,7 @@ import { useContext, useState } from 'react';
 
 import { DataContext } from "../context/DataProvider";
 import { Item } from './Item';
+import { BiSearch } from 'react-icons/bi';
 
 const Home = () => {
     const value = useContext(DataContext);
@@ -11,56 +12,54 @@ const Home = () => {
     const [loading] = value.loading;
 
     const [search, setSearch] = useState('');
-  
-    
-  
+
+
+
     const handleSearch = e => {
-      setSearch(e.target.value);
+        setSearch(e.target.value);
     }
 
-  
-    const filteredProducts = productos.filter(product => 
-          product.category.toLowerCase().includes(search.toLowerCase()) || product.title.toLowerCase().includes(search.toLowerCase())
-      );
 
-
-
-
+    const filteredProducts = productos.filter(product =>
+        product.category.toLowerCase().includes(search.toLowerCase()) || product.title.toLowerCase().includes(search.toLowerCase())
+    );
 
 
     return (
         <>
-        <SideCart />
-           
-        <div className='filtros-container'>
-            Buscar:   
+            <SideCart />
 
-            <input placeholder="Busca en el título o categoría" onChange={handleSearch} />
-            {/* <Link to="/"  className='filter-btn active'><BiSortAZ /></Link> 
-            <Link to="/" className='filter-btn'><BiSortZA /></Link> */}
-        </div>
-        <div className='container'>
-         
-        <div className="list-container">
-                {!loading && filteredProducts ? filteredProducts.map(item => (
-                    <Item
-                        key={item.id}
-                        id={item.id}
-                        title={item.title}
-                        description={item.description}
-                        price={item.price}
-                        category={item.category}
-                        image={item.image}
+            <div className='filtros-container'>
+            <div className="form-group search-form">
+                    <label  htmlFor="search">Buscar:</label>
+                <span className='search-icon'><BiSearch /></span>
+                    <input placeholder="Busca en el título o categoría..." onChange={handleSearch} id="search" className="form-control" />
+                    
+                </div>
 
-                    />)) : (
-                        
-                          "Cargando..."
-                        
-                      )}
             </div>
-        </div>
+            <div className='container'>
+
+                <div className="list-container">
+                    {!loading && filteredProducts ? filteredProducts.map(item => (
+                        <Item
+                            key={item.id}
+                            id={item.id}
+                            title={item.title}
+                            description={item.description}
+                            price={item.price}
+                            category={item.category}
+                            image={item.image}
+
+                        />)) : (
+
+                        "Cargando..."
+
+                    )}
+                </div>
+            </div>
         </>
-            )
+    )
 }
 
-            export default Home;
+export default Home;
